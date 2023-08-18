71c809761e17f65cba0647f785ddc9b24486cfc7
T. Lanette <TLPollard4516@gmail.com>
2023-08-18 14:23:05 -0400

    Added JS for slider
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/app.js
+++ js/app.js
@@ -24,3 +24,25 @@ $(document).ready(function () {
 
 });
 
+// Projects Slider
+$('#projects-slider').owlCarousel({
+    loop: true,
+    nav: false,
+    items: 2,
+    dots: true,
+    smartSpeed: 600,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2,
+            margin: 8,
+        }
+    }
+})
+
+//Reviews Slider 
